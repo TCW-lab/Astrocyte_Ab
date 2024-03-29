6aa9ced52ca77d2939b41479a34f3f6827a2a35d
georgewu25 <119002056+georgewu25@users.noreply.github.com>
2024-03-29 16:45:23 -0400

    Delete results/scripts directory
    Will push to scripts instead of results/scripts

--- results/scripts/run_wgcna_plot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-Rscript /projectnb/tcwlab/LabMember/mwu/Project/Astrocyte_Ab/2X100/results/scripts/WGCNA_plot.R
--- results/scripts/run_wgcna_sft.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-Rscript /projectnb/tcwlab/LabMember/mwu/Project/Astrocyte_Ab/2X100/results/scripts/WGCNA_sft.R
